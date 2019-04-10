c9e553c714e010bdb1a0acb6ea99af104cb77f5d
Keyuan Lu <11510052@mail.sustc.edu.cn>
2019-04-10 14:37:10 +0800

    Update some jsps

--- src/main/java/Servlet/JumpServlet.java
+++ src/main/java/Servlet/JumpServlet.java
@@ -1,7 +1,47 @@
 package Servlet;
 
+import DAO.QuestionDao;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JumpServlet extends HttpServlet {
-
+    private static final long serialVersionUID = 1L;
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                request.setCharacterEncoding("utf-8");
+                response.setContentType("text/html;charset=utf-8");
+                String question_id=request.getParameter("question_id");
+                if (question_id.equals("1")){
+                    request.getRequestDispatcher("jsp/adequately_testing.jsp").forward(request, response);;
+                    return;
+        }
+                if (question_id.equals("2")){
+                    request.getRequestDispatcher("jsp/chaining_test.jsp").forward(request, response);;
+                    return;
+                }
+                if (question_id.equals("3")){
+                    request.getRequestDispatcher("jsp/expected_result.jsp").forward(request, response);;
+                    return;
+                }
+                if (question_id.equals("4")){
+                    request.getRequestDispatcher("jsp/named_correctly.jsp").forward(request, response);;
+                    return;
+                }
+                if (question_id.equals("5")){
+                    request.getRequestDispatcher("jsp/specific_value.jsp").forward(request, response);;
+                    return;
+                }
+                if (question_id.equals("6")){
+                    request.getRequestDispatcher("jsp/test_private_method.jsp").forward(request, response);;
+                    return;
+                }
+                if (question_id.equals("7")){
+                    request.getRequestDispatcher("jsp/too_much_assertion.jsp").forward(request, response);;
+                    return;
+                }
+    }
 }
--- src/webapp/jsp/My_Puzzle.jsp
+++ src/webapp/jsp/My_Puzzle.jsp
@@ -12,6 +12,34 @@
 </head>
 <body>
 This is my puzzle
+<form action="JumpServlet" method="post">
+    <input type="hidden" name="question_id" value="1">
+    <input type="submit" value="No.1">
+</form>
+<form action="JumpServlet" method="post">
+    <input type="hidden" name="question_id" value="2">
+    <input type="submit" value="No.2">
+</form>
+<form action="JumpServlet" method="post">
+    <input type="hidden" name="question_id" value="3">
+    <input type="submit" value="No.3">
+</form>
+<form action="JumpServlet" method="post">
+    <input type="hidden"  name="question_id" value="4">
+    <input type="submit" value="No.4">
+</form>
+<form action="JumpServlet" method="post">
+    <input type="hidden" name="question_id" value="5">
+    <input type="submit" value="No.5">
+</form>
+<form action="JumpServlet" method="post">
+    <input type="hidden" name="question_id" value="6">
+    <input type="submit" value="No.6">
+</form>
+<form action="JumpServlet" method="post">
+    <input type="hidden" name="question_id" value="7">
+    <input type="submit" value="No.7">
+</form>
 <font color="red" size="2"> ${msg }</font>
 </body>
 </html>
